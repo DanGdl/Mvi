4c178925d7941bec36552a10a52d56e522274147
YDT_DOM\dan-gl <dan-gl@yit.co.il>
2021-06-21 10:44:57 +0300

    # view holder fix

--- adapter/src/main/java/com/mdgd/adapter/AbstractVH.kt
+++ adapter/src/main/java/com/mdgd/adapter/AbstractVH.kt
@@ -17,7 +17,7 @@ abstract class AbstractVH<T>(itemView: View) : RecyclerView.ViewHolder(itemView)
         bind(item)
     }
 
-    abstract fun bind(item: T)
+    protected open fun bind(item: T) {}
 
     protected fun hasModel() = model != null
 }
